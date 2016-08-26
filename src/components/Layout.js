96e66abeb9c0dad9cbda605fb4088e533cb6f58f
peters313 <eric.peters3@gmail.com>
2016-08-26 18:36:06 -0500

    adding header component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,23 +1,24 @@
 import React from "react";
 import Footer from "./Footer";
+import Header from "./Header";
 
 export default class Layout extends React.Component {
   constructor() {
     super();
-    // this.state = {
-    //   title: "Welcome",
-    // };
+    this.state = {
+      title: "Entry Point",
+    };
   }
 
-  // changeTitle(title) {
-  //   this.setState({title});
-  // }
+  changeTitle(title) {
+    this.setState({title});
+  }
 
   render() {
     return (
       <div>
         <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
-        <Footer />
+         <Footer />
       </div>
     );
   }
