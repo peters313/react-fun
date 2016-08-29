13a4bfe42187ccdd44da2cce6fc8d13fc180a638
peters313 <eric.peters3@gmail.com>
2016-08-28 19:45:11 -0500

    moving file structure around

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Footer from "/Footer";
-import Header from "/Header";
+import Footer from "./Footer";
+import Header from "./Header";
 
 export default class Layout extends React.Component {
   constructor() {
