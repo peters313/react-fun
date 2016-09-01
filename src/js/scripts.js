47db46ea87ba92731eb3c4d120437284fba2d967
peters313 <eric.peters3@gmail.com>
2016-09-01 15:15:39 -0500

    adds some routes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,4 +5,12 @@ import Layout from "./components/Layout";
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Featured}></IndexRoute>
+      <Route path="archives(/:article)" name="archives" component={Archives}></Route>
+      <Route path="settings" name="settings" component={Settings}></Route>
+    </Route>
+  </Router>,
+app);
