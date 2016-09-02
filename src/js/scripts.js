23215d2f2331bfaacfcdae333f3f5fdd9a29a48b
peters313 <eric.peters3@gmail.com>
2016-09-02 10:22:26 -0500

    adds missing file to import list

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,7 @@ import { Router, Route, IndexRoute, hashHistory } from "react-router";
 import Layout from "./components/Layout";
 import Featured from "./pages/Featured";
 import Archives from "./pages/Archives";
+import Setting from "./pages/Settings";
 
 const app = document.getElementById('app');
 
