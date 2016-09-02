4c70809ba856f8634ca1cdd387979914bceec2c3
peters313 <eric.peters3@gmail.com>
2016-09-02 10:21:22 -0500

    adding additional components

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 import Layout from "./components/Layout";
+import Featured from "./pages/Featured";
+import Archives from "./pages/Archives";
 
 const app = document.getElementById('app');
 
