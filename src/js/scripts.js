75361e73ddc9f9bcbbd7b615413c8ea2d12fae8f
peters313 <eric.peters3@gmail.com>
2016-09-03 19:55:20 -0500

    quick changes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 import Layout from "./components/Layout";
+
 import Featured from "./pages/Featured";
 import Archives from "./pages/Archives";
 import Settings from "./pages/Settings";
